ac56f7e4e36be4acfa40f2d7a880799ec200a181
Adedotun Olawale <adedotun.olawale@indicina.co>
2022-05-01 23:24:48 +0100

    set up jwt and user authentication

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
-@Controller('users')
-export class UsersController {}
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UsersService } from '../services/users.service';
+import CreateUserInput from '../input/createUser.input';
+import { User } from '../schema/users/user.schema';
+import { CurrentUser } from '../users.decorators';
+
+@ApiTags('users')
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly auth: UsersService) {}
+
+  /**
+   * Login a user using the email/password combination
+   */
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  @ApiResponse({ status: 201, description: 'user logged in.' })
+  @ApiResponse({ status: 401, description: 'unauthorized.' })
+  async login(@Req() request: Request): Promise<any> {
+    return this.auth.login(request.user);
+  }
+
+  /**
+   * Register a new user.
+   */
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  @Post('register')
+  @ApiResponse({ status: 201, description: 'user created.' })
+  @ApiResponse({ status: 401, description: 'unauthorized.' })
+  async register(@Body() input: CreateUserInput): Promise<User> {
+    return this.auth.registerUser(input);
+  }
+
+  /**
+   * Generate a refresh token
+   * @param request
+   */
+  @UseGuards(AuthGuard('jwt'))
+  @Post('token')
+  @ApiResponse({ status: 201, description: 'refresh token.' })
+  @ApiResponse({ status: 401, description: 'unauthorized.' })
+  async refreshAccessToken(@Req() request: Request): Promise<any> {
+    return this.auth.refreshAccessToken(request);
+  }
+
+  /**
+   * Get currently authenticated user
+   * @param resp
+   * @param user
+   */
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  @ApiResponse({ status: 201, description: 'current user data.' })
+  @ApiResponse({ status: 401, description: 'unauthorized.' })
+  async me(@CurrentUser() user: User): Promise<{ user: User }> {
+    const currentUser = await this.auth.getUser(user);
+    return {
+      user: currentUser,
+    };
+  }
+}
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,151 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+
+import { SCHEMAS } from '@vendor-app/core/constants';
+import { User } from '../schema/users/user.schema';
+import CreateUserInput from '../input/createUser.input';
+import UpdateUserInput from '../input/updateUser.input';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(SCHEMAS.USER) private UserModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  /**
+   * Validate the users credentials.
+   *
+   * This is useful for the login.
+   *
+   * @todo update the default return.
+   *
+   * @param email
+   * @param password
+   * @constructor
+   */
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.UserModel.findOne({ email })
+      .select('+password')
+      .exec();
+    // Check if password is correct
+    if (!user) {
+      throw new NotFoundException('user with this details do not exist.');
+    }
+
+    if (bcrypt.compareSync(password, user.password)) {
+      return user;
+    }
+
+    throw new NotFoundException(
+      'Invalid Password, Please enter the correct password.',
+    );
+  }
+
+  /**
+   * Register a new user.
+   *
+   * @param payload
+   */
+  public async registerUser(payload: CreateUserInput): Promise<any> {
+    try {
+      payload.password = bcrypt.hashSync(payload.password, 10);
+
+      const user: User = new this.UserModel(payload);
+      await user.save();
+
+      return {
+        userId: user._id,
+        isSeller: user.role === 'seller' ? true : false,
+      };
+    } catch (error) {
+      // @todo improve on the error handling. Return exact field with the error
+      if ('code' in error && error.code === 11000) {
+        return {
+          message: 'The user already exists',
+          code: error.code,
+        };
+      }
+      return {
+        ...error,
+      };
+    }
+  }
+
+  /**
+   * Sign in a new user
+   *
+   * @todo we should a different key for the refresh token jwt
+   * @param user
+   */
+  async login(user: User): Promise<any> {
+    const payload = {
+      id: user.id,
+      role: user.role,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload, { expiresIn: 3000 }),
+    };
+  }
+
+  /**
+   * Refresh an access token.
+   *
+   * For refresh tokens that have less than ten minutes before expiry a new refresh token is also
+   * added to the payload.
+   *
+   * @param request
+   */
+  async refreshAccessToken(request: any): Promise<any> {
+    const payload = {
+      id: request.user.id,
+    };
+
+    // Get expiring time
+    const exp = request.user.exp * 1000;
+    const timeLeft = Math.floor((exp - Date.now()) / 1000 / 60);
+
+    // Return if less than or equal 10 minutes;
+    if (timeLeft <= 10) {
+      return {
+        accessToken: this.jwtService.sign(payload, { expiresIn: 3000 }),
+        refreshToken: this.jwtService.sign(payload, { expiresIn: '1h' }),
+      };
+    }
+
+    return {
+      accessToken: this.jwtService.sign(payload, { expiresIn: 3000 }),
+    };
+  }
+
+  /**
+   * Get application roles.
+   *
+   * @todo Integrate with the roles guard.
+   */
+  async getUserByKey(key: string, value: string): Promise<any> {
+    const check = await this.UserModel.findOne({ [key]: value });
+
+    return check;
+  }
+
+  /**
+   * Get user
+   * @param user
+   */
+  async getUser(user: any): Promise<User> {
+    return await this.UserModel.findById(user.id).select('+password');
+  }
+
+  /**
+   * Get user
+   * @param user
+   */
+  async updateUser(id: string, data: UpdateUserInput): Promise<User> {
+    return this.UserModel.findByIdAndUpdate(id, data, { new: true }).exec();
+  }
+}
